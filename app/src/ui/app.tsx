1c0f48f9c56f89509820da7c963b0f0ca059579f
evelyn masso <outofambit@github.com>
2019-04-08 12:17:16 -0700

    make it Resizable

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -2182,6 +2182,7 @@ export class App extends React.Component<IAppProps, IAppState> {
           emoji={state.emoji}
           sidebarWidth={state.sidebarWidth}
           commitSummaryWidth={state.commitSummaryWidth}
+          stashedFilesWidth={state.stashedFilesWidth}
           issuesStore={this.props.issuesStore}
           gitHubUserStore={this.props.gitHubUserStore}
           onViewCommitOnGitHub={this.onViewCommitOnGitHub}
